349d3394b12a7b956ed9e590f62096e54906bfd0
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-29 12:34:07 +0100

    feat: "static layout"

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,20 @@ const { createApp } = Vue
 createApp({
 data() {
     return {
-        
+        tasks: [
+            {
+                text: 'Fare i compiti',
+                done: false,
+            },
+            {
+                text: 'Fare la spesa',
+                done: false,
+            },
+            {
+                text: 'Portare al parco i cani',
+                done: false,
+            },
+        ]
     }
 },
 }).mount('#app')
